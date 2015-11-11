46f0aa0c2aa5eca1c982cfd463175d974d101bf2
Michael Curtis <michaelcurtis@Michaels-MacBook-Pro.local>
2015-11-11 17:03:14 -0500

    Addded if statement

--- color_counter/color_counter.vhd
+++ color_counter/color_counter.vhd
@@ -21,22 +21,24 @@ begin
 			next_color <= "000";
 			
 		-- Otherwise we change on the rising edge the output to the next value
-		elsif rising_edge(clock) and enable and not(reset) then
-			case present_color is
-				when "000" => next_color <= "001";
-				when "001" => next_color <= "010";
-				when "010" => next_color <= "011";
-				when "011" => next_color <= "100";
-				when "100" => next_color <= "101";
-				when "101" => next_color <= "000";
-			end case;
+		elsif rising_edge(clock) 
+			if and enable and not(reset) then
+				case present_color is
+					when "000" => next_color <= "001";
+					when "001" => next_color <= "010";
+					when "010" => next_color <= "011";
+					when "011" => next_color <= "100";
+					when "100" => next_color <= "101";
+					when "101" => next_color <= "000";
+				end case;
 			
-			-- Now that we are changing the value we need to address the 101 case to allow for cascading
-			if present_color = "100" then
-				last_reached = "1";
-			else
-				last_reached = "0";
-				
+			
+				-- Now that we are changing the value we need to address the 101 case to allow for cascading
+				if present_color = "100" then
+					last_reached = "1";
+				else
+					last_reached = "0";
+			end if;
 		end if;
 	end process
 end architecture;
