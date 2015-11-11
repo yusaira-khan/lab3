d00eaf98b5c2eafe5b2c91ea908e20b9616c23a0
Michael Curtis <michaelcurtis@Michaels-MacBook-Pro.local>
2015-11-11 15:28:17 -0500

    Finished color_counter logic to cascade in higher level of abstraction

--- color_counter/color_counter.vhd
+++ color_counter/color_counter.vhd
@@ -1,9 +1,42 @@
+-- Here we define our color_counter that acts like a counter except it returns to zero and sets last_reached = 1 on 6 instead of 8 (ie, 101 instead of 111)
 entity color_counter is
 	port (	present_color	: in std_logic_vector(2 downto 0);
 			clock 			: in std_logic;
 			reset			: in std_logic;
+			enable			: in std_logic;
 			last_reached	: out std_logic;
 			next_color 		: out std_logic_vector(2 downto 0);
 end color_counter;
 
+-- Initialize behavior
 architecture behavior of color_counter is
+begin
+		
+	-- Initialie process
+	process(present_color, clock, reset, enable)
+	begin
+	
+		-- If we have a reset set back to 000
+		if reset then 
+			next_color <= "000";
+			
+		-- Otherwise we change on the rising edge the output to the next value
+		elsif rising_edge(clock) and enable and not(reset) then
+			case present_color is
+				when "000" => next_color <= "001";
+				when "001" => next_color <= "010";
+				when "010" => next_color <= "011";
+				when "011" => next_color <= "100";
+				when "100" => next_color <= "101";
+				when "101" => next_color <= "000";
+			end case;
+			
+			-- Now that we are changing the value we need to address the 101 case to allow for cascading
+			if present_color = "100" then
+				last_reached = "1";
+			else
+				last_reached = "0";
+				
+		end if;
+	end process
+end architecture;
--- color_counter/g24_possibility_table.vhd
+++ color_counter/g24_possibility_table.vhd
@@ -14,54 +14,16 @@ architecture behavior of g24_possibility_table is
 	
 	signal TC : std_logic_vector(11 downto 0);
 	signal pin_index : std_logic vector(1 downto 0);
+	-- Declare componenet
 	
 begin
 	
 	TC <= "000000000000"
-	pin_index <= "00"
-	
-	TC0 <= TC(2 downto 0);
-	TC1 <= TC(5 downto 3);
-	TC2 <= TC(8 downto 6);
-	TC3 <= TC(11 downto 9);
-	
 	
 	process(TC_RST, TC_EN, CLK, TC)
 	begin
-		signal increment : std_logic_vector(11 downto 0);
+				
 		
-		-- Based on which pin we are searching possible values for we will have a different incrementing value
-		case pin_index is
-			when "00" => increment <= "000000000001";
-			when "01" => increment <= "000000001000";
-			when "10" => increment <= "000001000000";
-			when "11" => increment <= "001000000000";
-		end case;
-		
-		increment <= "000000000001";
-		
-		-- The count value TC is set to zero ("000000000000") when TC_RST is high
-		if TC_RST then 
-			TC <= "000000000000";
-		-- If TC_RST is low and TC_EN is high, then TC is set to the next count value in the count sequence on the next positive-going transition of the CLK signal
-		elsif rising_edge(CLK) and TC_EN and not(TC_RST) then
-			TC <= TC + increment;
-		end if;
-		
-		-- Need to update the pin index if we reach the end of that search field
-		case TC is
-			when "000000000101" => pin_index <= "01";
-			when "000000101101" => pin_index <= "10";
-			when "000101101101" => pin_index <= "11";
-			-- Iterate around to the first if we see the end
-			when "101101101101" => pin_index <= "00";
-		end case;
-		
-		-- If the count value TC has a value of "101101101101" the signal TC_LAST goes high, TC is not changed. Otherwise TC_LAST is low.
-		if TC = "101101101101" then
-			TC_LAST <= "1";
-		else
-			TC_LAST <= "0";
 		
 	end process;
 
