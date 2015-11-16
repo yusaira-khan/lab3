156c12850c859c4fbfef0a1929b6d477348352a0
Michael Curtis <michaelcurtis@Michaels-MacBook-Pro.local>
2015-11-16 14:26:12 -0500

    Removed present state variable

--- color_counter/color_counter.vhd
+++ color_counter/color_counter.vhd
@@ -4,8 +4,7 @@
 library ieee; -- allows use of the std_logic_vector type 
 use ieee.std_logic_1164.all; 
 entity color_counter is
-	port (	present_color	: in std_logic_vector(2 downto 0);
-			clock 			: in std_logic;
+	port (	clock 			: in std_logic;
 			reset			: in std_logic;
 			enable			: in std_logic;
 			last_reached	: out std_logic;
@@ -14,20 +13,21 @@ end color_counter;
 
 -- Initialize behavior
 architecture behavior of color_counter is
+	
 begin
 		
 	-- Initialie process
-	process(present_color, clock, reset, enable)
+	process(clock, reset, enable)
 	begin
-	
-		-- If we have a reset set back to 000
+		
+		-- If we have a reset set back to 000, need to signify on start
 		if reset = '1' then 
 			next_color <= "000";
-			
+		
 		-- Otherwise we change on the rising edge the output to the next value
 		elsif rising_edge(clock)  then
 			if enable = '1' and reset = '0' then
-				case present_color is
+				case next_color is
 					when "000" => next_color <= "001";
 					when "001" => next_color <= "010";
 					when "010" => next_color <= "011";
@@ -35,7 +35,6 @@ begin
 					when "100" => next_color <= "101";
 					when "101" => next_color <= "000";
 				end case;
-			
 			
 				-- Now that we are changing the value we need to address the 101 case to allow for cascading
 				if present_color = "100" then
